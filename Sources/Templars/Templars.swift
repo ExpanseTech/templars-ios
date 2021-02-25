00aa3c0a0d1987848fd48450431fc0b53860d13b
Chukwuemeka <matthew.chukwuemeka40@gmail.com>
2021-02-25 01:10:15 +0100

    make templars properties public

--- Sources/Templars/Templars.swift
+++ Sources/Templars/Templars.swift
@@ -11,8 +11,11 @@ import Combine
 
 @available(OSX 10.15, *)
 @available(iOS 13.0, *)
+
+/// Main Class
 public class Templars {
     
+    /// API key
     let apiKey: String
     let networkHelper: NetworkHelper
     
